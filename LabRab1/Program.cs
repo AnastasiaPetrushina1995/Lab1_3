77fa831ef961a8b4c69c201d2bcd6fde57cef202
AnastasiaPetrushina1995 <anastasiapetrushina1995@mail.ru>
2015-12-03 16:23:33 +0600

    удалили

--- LabRab1/Program.cs
+++ LabRab1/Program.cs
@@ -19,24 +19,13 @@ namespace GUI
             /*Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new HeadForm()); */
-          /*  try
-            {
-                ISalaryCalculator[] Salary = new ISalaryCalculator[3];
-                Salary[0] = new Hours(2, 4);
-                Salary[1] = new Month(2, 4, 6);
-                Salary[2] = new Rate(5, 2);
-                /*List <ISalaryCalculator> salary = new List <ISalaryCalculator>();
-                salary.Add(new Hours);*/
-
-               // for (int i = 0; i < 3; i++)
             try
             {
-                List< ISalaryCalculator> Salary = new List<ISalaryCalculator>(); // комментарий 
+                List <ISalaryCalculator> Salary = new List<ISalaryCalculator>(); // комментарий 
                 Salary.Add(new Hours(2, 4));
                 Salary.Add(new Month(2, -4, 6));
                 Salary.Add(new Rate(5, 2));
                 foreach (ISalaryCalculator fig in Salary)
-            
                 {
                     Console.WriteLine(fig.Calculate());
                 }
